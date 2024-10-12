a15ebd221eaac49a8d29a80e27a1ed6aa9c85d53
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-10-12 18:36:49 +0500

    response text update

--- handlers/private/free_lesson_participant.py
+++ handlers/private/free_lesson_participant.py
@@ -12,7 +12,7 @@ from states import Registration
 @dp.message(StateFilter(Registration), PrivateFilter(), F.text == "✅ Ha, bepul ochiq darsga qatnashib ko'raman")
 async def set_free_lesson_participant(message: Message, state: FSMContext):
     await db.survey_update_freeLessonParticipation(message.from_user.id, 'yes')
-    await message.answer("Zo'r! Sizni shanba kuni soat 15:00 da bo'lib o'tadigan ochiq darsimizga yozib qo'ydim. O'zingiz bilan yaxshi kayfiyatni olib kelishingiz kifoya.",
+    await message.answer("Zo'r! Operatorlarimiz yaqin vaqt ichida siz bilan bog'lanib, ochiq dars kuni va vaqti haqida ma'lumot berishadi. Kuningiz xayrli o'tsin! ❤️",
                          reply_markup=await main_menu(telegramId=message.from_user.id))
     await state.clear()
     await message.delete()
@@ -21,7 +21,7 @@ async def set_free_lesson_participant(message: Message, state: FSMContext):
 @dp.message(StateFilter(Registration), PrivateFilter(), F.text == "❌ Yo'q, hozircha kerak emas")
 async def set_free_lesson_participant_no(message: Message, state: FSMContext):
     await db.survey_update_freeLessonParticipation(message.from_user.id, 'no')
-    await message.answer("So'rovnomada ishtirok etganingiz uchun rahmat!",
+    await message.answer("Shunday bo'lsada, Unicum Academy ijtimoiy tarmoqlaridagi sahifalarga a'zo bo'lib, yangiliklardan xabardor bo'lib turishingiz mumkin: https://t.me/unicum_academy, https://www.instagram.com/unicum_academy",
                          reply_markup=await main_menu(telegramId=message.from_user.id))
     await state.clear()
     await message.delete()
