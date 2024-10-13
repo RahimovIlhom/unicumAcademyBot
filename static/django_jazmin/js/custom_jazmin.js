dc2bc6d116916c5bcc57d12de1744b9ddbf89631
Sunnat <127438889+sunnatila@users.noreply.github.com>
2024-10-13 21:11:13 +0500

    custom js updated

--- static/django_jazmin/js/custom_jazmin.js
+++ static/django_jazmin/js/custom_jazmin.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navbarToggler = document.querySelector('.navbar-toggler');
-    const navbarCollapse = document.querySelector('.navbar-collapse');
-
-    if (navbarToggler) {
-        navbarToggler.addEventListener('click', function () {
-            navbarCollapse.classList.toggle('show');
-        });
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    var navItem = document.querySelectorAll('.nav-item.d-none');
+    navItem.forEach((item)=>{
+        if(item){
+            item.classList.remove('d-none');
+        }
+    })
 });
