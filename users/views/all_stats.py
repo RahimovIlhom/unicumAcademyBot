9b54d40477d95ec245e89375b15cc14d9fabc9bc
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-10-22 15:59:06 +0500

    stats names updates

--- users/views/all_stats.py
+++ users/views/all_stats.py
@@ -4,13 +4,14 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from users.models import Survey
-from users.serializers.stats_serializers import FieldStatsSerializer, StatsListSerializer
+from users.models import Survey, GENDERS, COURSE_NUMBERS, EDUCATION_TYPES, EDUCATION_DIRECTIONS, ENGLISH_LEVELS, GOALS, \
+    DAYS_PER_WEEK_CHOICES, LEARNING_EXPERIENCE_CHOICES, START_LEARNING_IMPORTANCE_CHOICES, ENGLISH_PROFICIENCY_CHOICES, \
+    COURSE_TYPES, CONSIDER_ENROLLMENT_CHOICES, FREE_LESSON_PARTICIPATION_CHOICES, OBSTACLE_CHOICES, CONDITIONS_CHOICES
 
 
 class CombinedStatsAPIView(APIView):
 
-    def get_field_stats(self, field_name):
+    def get_field_stats(self, field_name, stats_dict: dict = {}):
         total_count = Survey.objects.count()
 
         # Null qiymatlar uchun "None" ni o'z ichiga oluvchi ma'lumotlarni olib kelamiz
@@ -24,7 +25,7 @@ class CombinedStatsAPIView(APIView):
         # Natijalar ro'yxati
         stats = [
             {
-                "name": item.get(field_name),
+                "name": stats_dict.get(item.get(field_name), 'no_data'),
                 "count": item["count"],
                 "percentage": round((item["count"] / total_count) * 100, 2) if total_count else 0
             }
@@ -45,7 +46,7 @@ class CombinedStatsAPIView(APIView):
             "stats": stats
         }
 
-    def get_multi_choice_stats(self, field_name):
+    def get_multi_choice_stats(self, field_name, stats_dict: dict = {}):
         # Barcha maydonlardagi tanlangan qiymatlarni olish
         field_values = Survey.objects.values_list(field_name, flat=True).distinct()
 
@@ -63,7 +64,7 @@ class CombinedStatsAPIView(APIView):
 
         stats = [
             {
-                "name": key,
+                "name": stats_dict.get(key, 'no_data'),
                 "count": count,
                 "percentage": round((count / user_count) * 100, 2) if user_count else 0
             }
@@ -129,21 +130,21 @@ class CombinedStatsAPIView(APIView):
     )
     def get(self, request):
         response_data = {
-            "gender": self.get_field_stats("gender"),
-            "courseNumber": self.get_field_stats("courseNumber"),
-            "educationType": self.get_field_stats("educationType"),
-            "educationDirection": self.get_field_stats("educationDirection"),
-            "englishLevel": self.get_field_stats("englishLevel"),
-            "englishGoal": self.get_field_stats("englishGoal"),
-            "daysPerWeek": self.get_field_stats("daysPerWeek"),
-            "learningExperience": self.get_field_stats("learningExperience"),
-            "obstacles": self.get_multi_choice_stats("obstacles"),
-            "conditions": self.get_multi_choice_stats("conditions"),
-            "startLearning_importance": self.get_field_stats("startLearning_importance"),
-            "englishProficiency": self.get_field_stats("englishProficiency"),
-            "courseType": self.get_field_stats("courseType"),
-            "considerEnrollment": self.get_field_stats("considerEnrollment"),
-            "freeLessonParticipation": self.get_field_stats("freeLessonParticipation"),
+            "gender": self.get_field_stats("gender", dict(GENDERS)),
+            "courseNumber": self.get_field_stats("courseNumber", dict(COURSE_NUMBERS)),
+            "educationType": self.get_field_stats("educationType", dict(EDUCATION_TYPES)),
+            "educationDirection": self.get_field_stats("educationDirection", dict(EDUCATION_DIRECTIONS)),
+            "englishLevel": self.get_field_stats("englishLevel", dict(ENGLISH_LEVELS)),
+            "englishGoal": self.get_field_stats("englishGoal", dict(GOALS)),
+            "daysPerWeek": self.get_field_stats("daysPerWeek", dict(DAYS_PER_WEEK_CHOICES)),
+            "learningExperience": self.get_field_stats("learningExperience", dict(LEARNING_EXPERIENCE_CHOICES)),
+            "obstacles": self.get_multi_choice_stats("obstacles", dict(OBSTACLE_CHOICES)),
+            "conditions": self.get_multi_choice_stats("conditions", dict(CONDITIONS_CHOICES)),
+            "startLearning_importance": self.get_field_stats("startLearning_importance", dict(START_LEARNING_IMPORTANCE_CHOICES)),
+            "englishProficiency": self.get_field_stats("englishProficiency", dict(ENGLISH_PROFICIENCY_CHOICES)),
+            "courseType": self.get_field_stats("courseType", dict(COURSE_TYPES)),
+            "considerEnrollment": self.get_field_stats("considerEnrollment", dict(CONSIDER_ENROLLMENT_CHOICES)),
+            "freeLessonParticipation": self.get_field_stats("freeLessonParticipation", dict(FREE_LESSON_PARTICIPATION_CHOICES)),
             "age": self.get_simple_value_stats("age"),
             "importanceRanking": self.get_simple_value_stats("importanceRanking")
         }
